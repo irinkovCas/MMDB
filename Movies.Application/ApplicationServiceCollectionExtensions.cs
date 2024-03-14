18d39797c97e515e6e85eb15e69f3d4ccc639902
irinkovCas <97587356+irinkovCas@users.noreply.github.com>
2024-03-14 17:11:15 +0200

    fix: change lifetimes from singletons to scoped

--- Movies.Application/ApplicationServiceCollectionExtensions.cs
+++ Movies.Application/ApplicationServiceCollectionExtensions.cs
@@ -10,16 +10,16 @@ namespace Movies.Application {
     public static class ApplicationServiceCollectionExtensions {
 
         public static IServiceCollection AddApplication(this IServiceCollection services) {
-            services.AddSingleton<IMovieService, MovieService>();
-            services.AddSingleton<IMovieRepository, MovieRepository>(); // Singleton because of validation
-            services.AddValidatorsFromAssemblyContaining<IApplicationMarker>(ServiceLifetime.Singleton);
+            services.AddScoped<IMovieService, MovieService>();
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddValidatorsFromAssemblyContaining<IApplicationMarker>(ServiceLifetime.Scoped);
 
             return services;
         }
 
         public static IServiceCollection AddDatabase(this IServiceCollection services, string connectionString) {
 
-            services.AddDbContext<MovieDbContext>(options => options.UseNpgsql(connectionString), ServiceLifetime.Singleton);
+            services.AddDbContext<MovieDbContext>(options => options.UseNpgsql(connectionString), ServiceLifetime.Scoped);
 
             return services;
         }
